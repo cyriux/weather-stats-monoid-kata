9a281df4a25d65ea988e7d75b5946045494da651
Pierre IRRMANN <pirrmann@pirrmann.net>
2013-05-04 15:55:47 +0200

    Implementation of this kata in C#
    An attempt in C#, by @pirrmann.
    New types and stats can be generated using T4 templates.

---
+++ WeatherKataCSharp/Implementation/Foldable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WeatherKata
+{
+    public interface IFoldable<T>
+    {
+        T Value { get; }
+        IFoldable<T> Plus(IFoldable<T> other);
+        IFoldable<T> Minus(IFoldable<T> other);
+        IFoldable<T> DividedBy(int d);
+        IFoldable<T> Squared();
+        IFoldable<T> GetSquareRoot();
+    }
+
+    public partial class Fold
+    {
+        public static IFoldable<T> From<T>(T value)
+        {
+            dynamic foldFactory = new Fold();
+            dynamic dv = (dynamic)value;
+            return (IFoldable<T>)foldFactory.BuildFrom(dv);
+        }
+    }
+}
